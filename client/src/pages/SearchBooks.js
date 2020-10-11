5e3646c9d1eedb954fe273b670dbf0db07fe0a41
shhu21 <hu.sara21@gmail.com>
2020-10-11 03:16:50 -0700

    adds remove book and user functionality to savedbook.js

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -4,8 +4,11 @@ import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'reac
 import Auth from '../utils/auth';
 import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+import { useMutation } from '@apollo/react-hooks';
+import { SAVE_BOOK } from '../utils/mutations';
 
 const SearchBooks = () => {
+  const [savedBook] = useMutation(SAVE_BOOK);
   // create state for holding returned google api data
   const [searchedBooks, setSearchedBooks] = useState([]);
   // create state for holding our search field data
@@ -65,16 +68,11 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      // if book successfully saves to user's account, save book id to state
-      setSavedBookIds([...savedBookIds, bookToSave.bookId]);
-    } catch (err) {
-      console.error(err);
+      await savedBook({
+        variables: { bookId: bookId }
+      });
+    } catch (e) {
+      console.error(e);
     }
   };
 
